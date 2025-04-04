d14b5266bc5084e93093a013b2ce0a79044e5d3f
junaidshah2940 <shahjunaid1994@gmail.com>
2025-04-04 11:14:53 +0330

    added homepage

--- OncoNet/settings.py
+++ OncoNet/settings.py
@@ -56,7 +56,7 @@ ROOT_URLCONF = 'OncoNet.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
