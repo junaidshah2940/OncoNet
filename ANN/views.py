3984953227354688f28093d81d6efad7f33776b8
junaidshah2940 <shahjunaid1994@gmail.com>
2025-04-05 13:43:42 +0330

    added form of data

--- ANN/views.py
+++ ANN/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from .forms import DataInputForm
 
 # Create your views here.
+
+def data_form_view(request):
+    if request.method == 'POST':
+        form = DataInputForm(request.POST)
+        if form.is_valid():
+            pass
+    else:
+        form = DataInputForm()
+    return render(request, 'form.html', {'form': form})
