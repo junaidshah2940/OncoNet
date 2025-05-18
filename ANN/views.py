a7dc2abee577d1abe9067a978426e716cdb8303b
junaidshah2940 <shahjunaid1994@gmail.com>
2025-05-19 03:03:38 +0330

    added deploy smart contract

--- ANN/views.py
+++ ANN/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from .forms import DataInputForm
 from .model import classify_data
 from BC.blockchain import record_classification
-from BC.deploy import deploy_contract
+# from BC.deploy import deploy_contract
 
 def data_form_view(request):
     if request.method == 'POST':
@@ -14,17 +14,16 @@ def data_form_view(request):
             # Blockchain hash + store
             patient_id = data['patient_id']
 
-            # Use deploy_contract to load the ABI and address if not already loaded
-            contract_address, abi = deploy_contract()
-
             # Store the result in the blockchain
-            tx_hash = record_classification(patient_id, result)
+            res, contract_address, abi, bytecode = record_classification(patient_id, result)
 
             return render(request, 'result.html', {
                 'result': result,
-                'tx_hash': tx_hash,
+                'res': res,
                 'contract_address': contract_address,
                 'patient_id': patient_id,
+                'abi': abi,
+                "bytecode": bytecode
             })
     else:
         form = DataInputForm()
--- BC/blockchain.py
+++ BC/blockchain.py
@@ -1,32 +1,80 @@
-from web3 import Web3
-import hashlib
-import datetime
 import json
+from solcx import compile_source, install_solc
+from web3 import Web3
+from dotenv import load_dotenv
+import os
+from pathlib import Path
 
-# Connect to local blockchain (Ganache)
-w3 = Web3(Web3.HTTPProvider('http://127.0.0.1:7545'))
-account = w3.eth.accounts[0]
 
-# Load ABI and address from files
-def load_contract():
-    with open('BC/contract_abi.json') as f:
-        abi = json.load(f)
+def record_classification(patient_id, result):
+    env_path = Path(__file__).resolve().parent.parent / '.env'
+    load_dotenv(dotenv_path=env_path)   
+    install_solc('0.8.0')
 
-    with open('BC/contract_address.txt') as f:
-        contract_address = f.read().strip()
+    contract_source = '''
+    pragma solidity ^0.8.0;
+
+    contract DataStorage {
+        string public data;
+
+        function store(string memory _data) public {
+            data = _data;
+        }
+
+        function get() public view returns (string memory) {
+            return data;
+        }
+    }
+    '''
+
+    compiled_sol = compile_source(
+        contract_source,
+        output_values=['abi', 'bin'],
+        solc_version='0.8.0'
+    )
+    contract_id, contract_interface = compiled_sol.popitem()
+    abi = contract_interface['abi']
+    bytecode = contract_interface['bin']
+
+    w3 = Web3(Web3.HTTPProvider("http://127.0.0.1:8545"))
+    assert w3.is_connected(), "Failed to connect to Ganache"
+
+    private_key = os.getenv("PRIVATE_KEY")
+    account = w3.eth.account.from_key(private_key)
+    sender_address = account.address
+
+    DataStorage = w3.eth.contract(abi=abi, bytecode=bytecode)
+
+    nonce = w3.eth.get_transaction_count(sender_address)
+    tx = DataStorage.constructor().build_transaction({
+        'from': sender_address,
+        'nonce': nonce,
+        'gas': 2000000,
+        'gasPrice': w3.to_wei('50', 'gwei')
+    })
+
+    signed_tx = w3.eth.account.sign_transaction(tx, private_key)
+    tx_hash = w3.eth.send_raw_transaction(signed_tx.raw_transaction)
+    tx_receipt = w3.eth.wait_for_transaction_receipt(tx_hash)
+
+    contract_address = tx_receipt.contractAddress
 
     contract = w3.eth.contract(address=contract_address, abi=abi)
-    return contract
 
-# Generate hash of the classification data
-def hash_classification(patient_id, result):
-    data = f'{patient_id}_{result}_{datetime.datetime.utcnow()}'
-    return Web3.toBytes(hexstr=hashlib.sha256(data.encode()).hexdigest())
+    my_dict = {"patient_id": patient_id, "result": result}
+    json_str = json.dumps(my_dict)
 
-# Store classification result on the blockchain
-def record_classification(patient_id, result):
-    contract = load_contract()  # Use the loaded contract
-    data_hash = hash_classification(patient_id, result)
-    tx_hash = contract.functions.storeHash(data_hash).transact({'from': account})
-    receipt = w3.eth.wait_for_transaction_receipt(tx_hash)
-    return tx_hash.hex()
+    nonce = w3.eth.get_transaction_count(sender_address)
+    store_txn = contract.functions.store(json_str).build_transaction({
+        'from': sender_address,
+        'nonce': nonce,
+        'gas': 100000,
+        'gasPrice': w3.to_wei('50', 'gwei')
+    })
+
+
+    signed_store_txn = w3.eth.account.sign_transaction(store_txn, private_key)
+    store_tx_hash = w3.eth.send_raw_transaction(signed_store_txn.raw_transaction)
+    w3.eth.wait_for_transaction_receipt(store_tx_hash)
+    return ("Data stored on blockchain.", contract_address, abi, bytecode)
+
